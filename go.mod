2b93ed986a8943146fa0290514d8c65bcced0c39
Ernesto Rodriguez <ernesto.antonio.rod@gmail.com>
2023-07-06 19:26:43 -0500

    Installed new dependencies; created basic server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ernesto-rod/rssaggregator
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
@@ -13,9 +15,20 @@ func main() {
 
 	godotenv.Load(".env")
 
-	portSrting := os.Getenv("PORT")
-	if portSrting == "" {
+	portString := os.Getenv("PORT")
+	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
-	fmt.Println("Port:", portSrting)
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal()
+	}
 }
