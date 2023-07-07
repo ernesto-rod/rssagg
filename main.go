53aea6f63141a76d3ef9603fa7d92aa461633a0a
Ernesto Rodriguez <ernesto.antonio.rod@gmail.com>
2023-07-06 22:56:41 -0500

    Add Handler functions for healthz endpoint

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -20,6 +18,20 @@ func main() {
 		log.Fatal("PORT is not found in the environment")
 	}
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
